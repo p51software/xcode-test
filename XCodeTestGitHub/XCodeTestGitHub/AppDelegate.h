efdf2f29e8f9d439b902266358bace5ec0f36262
Patrick McGonigle <patrick@roundtripsoftware.com>
2014-12-14 16:18:26 -0600

    Created initial XCode project

---
+++ XCodeTestGitHub/XCodeTestGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCodeTestGitHub
+//
+//  Created by Patrick McGonigle on 12/14/14.
+//  Copyright (c) 2014 Patrick McGonigle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCodeTestGitHub/XCodeTestGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCodeTestGitHub
+//
+//  Created by Patrick McGonigle on 12/14/14.
+//  Copyright (c) 2014 Patrick McGonigle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
